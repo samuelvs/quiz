231e7f9aa1d290e29126e94f22ee1b8b23f0ed03
Samuel Veloso <samuelveloso42@gmail.com>
2024-02-13 23:28:20 -0300

    Adjust download

--- backend/src/routers/download.router.ts
+++ backend/src/routers/download.router.ts
@@ -1,12 +1,15 @@
-import express, { Router } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import { Router } from 'express';
 import path from 'path';
 import fs from 'fs';
 
 const router = Router();
+const files = process.env.prod === 'true' ? '../public/assets/' : '../assets/'
 
 router.get("/pdf/:filename", (req, res) => {
   const { filename } = req.params;
-  const filePath = path.join(__dirname, '../assets/', filename);
+  const filePath = path.join(__dirname, files, filename);
 
  if (fs.existsSync(filePath)) {
   res.setHeader('Content-Type', 'application/pdf');
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import { Request } from 'express';
 import cors from "cors";
 import dotenv from 'dotenv';
 dotenv.config();
